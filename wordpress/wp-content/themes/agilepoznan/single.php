35a616c3cf3ea7a77ade359d97b2fe8304969e9e
Marek Podsiadły <marek.podsiadly@nortpoint.io>
2016-10-19 12:09:48 +0200

    post title section

--- wordpress/wp-content/themes/agilepoznan/single.php
+++ wordpress/wp-content/themes/agilepoznan/single.php
@@ -13,8 +13,32 @@ while ( have_posts() ) : the_post();
 		) );
 	}
 
+	$meeting["date"] = simple_fields_value("data")["date_time_format"];
+	$meeting["place"] = simple_fields_value("miejsce");
+	$meeting["address1"] = simple_fields_value("adres1");
+	$meeting["address2"] = simple_fields_value("adres2");
+	$meeting["speaker"] = simple_fields_value("imienazwisko");
+	$meeting["company"] = simple_fields_value("firma");
+	$meeting["avatar"] = simple_fields_value("avatar")["image_src"]["thumbnail"];
+	$meeting["title"] = get_the_title();
+	$meeting["excerpt"] = get_the_excerpt();
+	$meeting["permalink"] = get_permalink();
+?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="post-header">
+		<div class="container">
+			<?php the_title( sprintf( '<h1 class="post-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+			<?php if ( $meeting['speaker'] ): ?>
+			<p class="post-author"><?php echo $meeting['speaker']; ?></p>
+			<?php else: ?>
+			<p class="post-author"><?php echo get_the_author(); ?></p>
+			<?php endif; ?>
+		</div>
+	</header>
+</article>
+<?php
 	// Include the single post content template.
-	get_template_part( 'template-parts/content', 'single' );
+	//get_template_part( 'template-parts/content', 'single' );
 
 
 endwhile;

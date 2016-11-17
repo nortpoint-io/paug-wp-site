687b71b0b1d42f108fb3e894e708eafa859071b5
Marek Podsiadły <marek.podsiadly@nortpoint.io>
2016-11-17 13:20:29 +0100

    redundant code removed

--- wordpress/wp-content/themes/agilepoznan/single.php
+++ wordpress/wp-content/themes/agilepoznan/single.php
@@ -21,35 +21,9 @@ while ( have_posts() ) : the_post();
 	$is_meeting = !empty($meeting["date"]) && in_category("spotkania");
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="post-header">
-		<div class="container">
-			<?php the_title( sprintf( '<h1 class="post-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
-			<?php if ( $is_meeting ): ?>
-			<p class="post-author"><?php echo $meeting['speaker']; ?> - <?php echo $meeting['date']; ?></p>
-			<?php else: ?>
-			<p class="post-author"><?php echo get_the_author(); ?></p>
-			<?php endif; ?>
-		</div>
-	</header>
-	<?php if ( has_excerpt() ): ?>
-		<div class="post-summary">
-			<div class="container thin">
-				<?php the_excerpt(); ?>
-			</div>
-		</div>
-	<?php endif; ?>
-	<?php if ( has_post_thumbnail() ): ?>
-		<div class="post-thumbnail">
-			<div class="container">
-				<?php the_post_thumbnail(); ?>
-			</div>
-		</div>
-	<?php endif; ?>
-	<div class="post-content">
-		<div class="container thin">
-			<?php the_content(); ?>
-		</div>
-	</div>
+	<?php
+	get_template_part( 'template-parts/content', get_post_format() );
+	?>
 	<?php if ( ( get_the_author_meta( 'description' ) && !$is_meeting) || ($meeting['bio'] && $is_meeting) ): ?>
 	<div class="gray-container">
 		<div class="container">

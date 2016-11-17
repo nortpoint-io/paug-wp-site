5f970afd16f9cf7277a3c09995334a197f73912b
Marek Podsiadły <marek.podsiadly@nortpoint.io>
2016-11-17 13:26:11 +0100

    single page layout

--- wordpress/wp-content/themes/agilepoznan/template-parts/content-page.php
+++ wordpress/wp-content/themes/agilepoznan/template-parts/content-page.php
@@ -9,37 +9,46 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
+	<header class="post-header">
+		<div class="container">
+			<?php the_title( sprintf( '<h1 class="post-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+		</div>
+	</header>
 
-	<?php twentysixteen_post_thumbnail(); ?>
+	<?php if ( has_post_thumbnail() ): ?>
+		<div class="post-thumbnail">
+			<div class="container">
+				<?php the_post_thumbnail(); ?>
+			</div>
+		</div>
+	<?php endif; ?>
 
-	<div class="entry-content">
-		<?php
-		the_content();
+	<div class="post-content">
+		<div class="container thin">
+			<?php
+			the_content();
 
-		wp_link_pages( array(
-			'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentysixteen' ) . '</span>',
-			'after'       => '</div>',
-			'link_before' => '<span>',
-			'link_after'  => '</span>',
-			'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'twentysixteen' ) . ' </span>%',
-			'separator'   => '<span class="screen-reader-text">, </span>',
-		) );
-		?>
-	</div><!-- .entry-content -->
+			wp_link_pages( array(
+				'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentysixteen' ) . '</span>',
+				'after'       => '</div>',
+				'link_before' => '<span>',
+				'link_after'  => '</span>',
+				'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'twentysixteen' ) . ' </span>%',
+				'separator'   => '<span class="screen-reader-text">, </span>',
+			) );
+			?>
+		</div>
+	</div>
 
 	<?php
-		edit_post_link(
+		/*edit_post_link(
 			sprintf(
-				/* translators: %s: Name of current post */
 				__( 'Edit<span class="screen-reader-text"> "%s"</span>', 'twentysixteen' ),
 				get_the_title()
 			),
 			'<footer class="entry-footer"><span class="edit-link">',
 			'</span></footer><!-- .entry-footer -->'
-		);
+		);*/
 	?>
 
-</article><!-- #post-## -->
+</article>

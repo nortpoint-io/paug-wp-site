84cd5eef0f0477a42a66b966eaf979f46d190ea5
Marek Podsiadły <marek.podsiadly@nortpoint.io>
2017-01-02 15:50:29 +0100

    footer changes

--- wordpress/wp-content/themes/agilepoznan/footer.php
+++ wordpress/wp-content/themes/agilepoznan/footer.php
@@ -1,6 +1,6 @@
 
 <footer>
-    Created by <a href="http://nortpoint.io"><img src="<?php bloginfo('template_directory') ?>/img/nortpoint_logo.png" alt="nortpoint.io"></a>
+    <a href="http://nortpoint.io">Created by <img src="<?php bloginfo('template_directory') ?>/img/nortpoint_logo.png" alt="nortpoint.io"></a>
 </footer>
 <?php wp_footer(); ?>
 </body>

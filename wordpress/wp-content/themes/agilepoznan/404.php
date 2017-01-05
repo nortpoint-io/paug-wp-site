b30b5cc8bafcde62bad82be2e3ab1b7ec3ca8f51
Marek Podsiadły <marek.podsiadly@nortpoint.io>
2017-01-05 13:16:44 +0100

    404 page changed

--- wordpress/wp-content/themes/agilepoznan/404.php
+++ wordpress/wp-content/themes/agilepoznan/404.php
@@ -9,26 +9,23 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<div class="main-content">
+	<section class="error-404 not-found">
+		<header class="post-header">
+			<div class="container">
+				<h1 class="post-title">Ooops! Nie znaleziono tej strony.</h1>
+			</div>
+		</header>
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'twentysixteen' ); ?></h1>
-				</header><!-- .page-header -->
+		<div class="post-content">
+			<div class="container thin">
+				<p>Wygląda na to, że nic nie znaleziono pod tym adresem. Może wypróbujesz wyszukiwarkę?</p>
 
-				<div class="page-content">
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentysixteen' ); ?></p>
+				<?php get_search_form(); ?>
+			</div>
+		</div>
+	</section>
+</div>
 
-					<?php get_search_form(); ?>
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
-		</main><!-- .site-main -->
-
-		<?php get_sidebar( 'content-bottom' ); ?>
-
-	</div><!-- .content-area -->
-
-<?php get_sidebar(); ?>
+<?php //get_sidebar(); ?>
 <?php get_footer(); ?>
